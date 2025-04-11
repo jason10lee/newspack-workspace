3add604704bbb11ab73e30945bedddbaa5d33efb
matticbot <semantic-release-bot@martynus.net>
2025-04-11 12:52:03 +0000

    chore(release): 3.11.4 [skip ci]
    ## [3.11.4](https://github.com/Automattic/newspack-newsletters/compare/v3.11.3...v3.11.4) (2025-04-11)
    
    ### Bug Fixes
    
    * **local-lists:** create contact if adding to local lists only ([#1801](https://github.com/Automattic/newspack-newsletters/issues/1801)) ([0d86016](https://github.com/Automattic/newspack-newsletters/commit/0d86016a6c7a4c0c193af46913ca9b0925030872))

--- plugins/newspack-newsletters/newspack-newsletters.php
+++ plugins/newspack-newsletters/newspack-newsletters.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         3.11.3
+ * Version:         3.11.4
  *
  * @package         Newspack_Newsletters
  */
