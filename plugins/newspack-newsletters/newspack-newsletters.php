a64331bf6aaedb0acf8a1ecac9da4038839d4c33
matticbot <semantic-release-bot@martynus.net>
2025-06-05 19:04:57 +0000

    chore(release): 3.15.1-alpha.1 [skip ci]
    ## [3.15.1-alpha.1](https://github.com/Automattic/newspack-newsletters/compare/v3.15.0...v3.15.1-alpha.1) (2025-06-05)
    
    ### Bug Fixes
    
    * dont check for current provider before init ([#1836](https://github.com/Automattic/newspack-newsletters/issues/1836)) ([547f975](https://github.com/Automattic/newspack-newsletters/commit/547f9751ec055fa2dc3dd66aa32f19e2ce5fd746))

--- plugins/newspack-newsletters/newspack-newsletters.php
+++ plugins/newspack-newsletters/newspack-newsletters.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         3.15.0
+ * Version:         3.15.1-alpha.1
  *
  * @package         Newspack_Newsletters
  */
