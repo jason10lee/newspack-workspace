9b07cc84cfb2d53450cd1f51957d98ba0794b0ac
matticbot <semantic-release-bot@martynus.net>
2025-12-11 15:06:04 +0000

    chore(release): 3.26.0-alpha.1 [skip ci]
    # [3.26.0-alpha.1](https://github.com/Automattic/newspack-newsletters/compare/v3.25.4...v3.26.0-alpha.1) (2025-12-11)
    
    ### Features
    
    * add support for WP 6.9's Notes feature to Newsletters ([#1975](https://github.com/Automattic/newspack-newsletters/issues/1975)) ([56352d0](https://github.com/Automattic/newspack-newsletters/commit/56352d01c868ef8c5918d4d8b337262829723cf8))
    * default mailchimp footer ([#1977](https://github.com/Automattic/newspack-newsletters/issues/1977)) ([dc4d435](https://github.com/Automattic/newspack-newsletters/commit/dc4d435504ef6a72f378376209c814b4b2fbb901))

--- plugins/newspack-newsletters/newspack-newsletters.php
+++ plugins/newspack-newsletters/newspack-newsletters.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * Text Domain:     newspack-newsletters
  * Domain Path:     /languages
- * Version:         3.25.4
+ * Version:         3.26.0-alpha.1
  *
  * @package         Newspack_Newsletters
  */
