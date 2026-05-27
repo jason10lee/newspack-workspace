f391c9b84d352f790baf1bce3c86903c0b16bde5
Adam Cassis <adam.cassis@automattic.com>
2026-05-27 14:26:44 +0200

    chore(republication-tracker-tool): baseline at last release [skip release]

--- plugins/republication-tracker-tool/release.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = require( '../../config/release' )( { name: 'republication-tracker-tool', phpFile: 'republication-tracker-tool.php' } );
--- plugins/republication-tracker-tool/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path = require( 'path' );
-const getBaseWebpackConfig = require( 'newspack-scripts/config/getWebpackConfig' );
-
-const entry = {
-	index: path.join( __dirname, 'src', 'index.js' ),
-};
-
-module.exports = getBaseWebpackConfig( { entry } );
