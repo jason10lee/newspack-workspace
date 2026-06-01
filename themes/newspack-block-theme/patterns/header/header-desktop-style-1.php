8f17aefa3d15df3402b58c5301a71cb8badeb2bc
Thomas Guillot <thomas@automattic.com>
2026-06-01 15:26:02 +0100

    style(block-theme): refine header triggers and simplify style 4 row

--- themes/newspack-block-theme/patterns/header/header-desktop-style-1.php
+++ themes/newspack-block-theme/patterns/header/header-desktop-style-1.php
@@ -37,7 +37,7 @@
 	<div class="wp-block-group alignwide">
 		<!-- wp:navigation {"className":"primary-navigation","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left","orientation":"horizontal","flexWrap":"wrap"},"style":{"typography":{"textTransform":"uppercase"}}} /-->
 
-		<!-- wp:newspack/overlay-search {"className":"is-style-icon-only","style":{"color":{"background":"#ffffff00"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"spacing":{"padding":{"right":"0","left":"0"}}},"textColor":"contrast"} /-->
+		<!-- wp:newspack/overlay-search {"className":"is-style-icon-only has-background-transparent","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"spacing":{"padding":{"right":"var:preset|spacing|20","left":"var:preset|spacing|20","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"textColor":"contrast"} /-->
 	</div>
 	<!-- /wp:group -->
 </div>
